db053721321792bb8b6e47cfbbe584c5053caab8
JayArindamMaity <maityjayarindam@gmail.com>
2025-07-23 12:36:30 +0530

    more changes

--- scripts/main.py
+++ scripts/main.py
@@ -20,9 +20,23 @@ This is the cli for managing the web data XD
 
 #these are all the variables for the data with their default value
 platform = ""
-ques_num = 0
+ques_num = ""
 ques_title = ""
 ques_link = ""
 sol_cpp = ""
 sol_python = ""
 sol_java = ""
+
+platform = input("Please enter the name of the platform: ")
+ques_num = input("Please enter the id/question number related to that platform: ")
+ques_title = input("Please enter the name/title of the problem wrt related platform: ")
+ques_type = input("""Please specify the programming language used to solve the related problem
+Valid types include:
+1. cpp
+2. java
+3. python
+You can type the name in any case or just select 1,2 or 3
+""")
+ques_link = input("Please provide the link to the related problem: ")
+
+print(platform)
